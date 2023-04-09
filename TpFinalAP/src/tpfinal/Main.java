43791e0c429d248e61bc2072419c5bad9a80cded
ArielVAlvarez <ftpalvarez@gmail.com>
2023-04-09 08:47:58 -0300

    agregado segunda entrega

--- TpFinalAP/src/tpfinal/Main.java
+++ TpFinalAP/src/tpfinal/Main.java
@@ -4,8 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Juego juego = new Juego();
-		juego.resolverJuego();
+		//no me deja lle el archivos donde esan los tuyos, no 
+		//entiendo porque puse una carpeta \ArgentinaProgramaTI01\archivos
+		//para que lo encuentre fijate si enconras el error.
+		
+		Participantes participantes = new Participantes();
+		
+		participantes.armarListaParticipantes("archivos/participantes.csv");
+		participantes.ListarParticipantes();
+		
+		//Juego juego = new Juego();
+		//juego.resolverJuego();
 
 	}
 
